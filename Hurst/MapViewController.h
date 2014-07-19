8c13f24934ca28385b82643c41d21d092ac5fa4e
Jordan Hudgens <jordan@wow.com>
2014-07-19 13:19:12 -0600

    integrated the mapkit and first test location

---
+++ Hurst/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  Hurst
+//
+//  Created by Jordan Hudgens on 7/19/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
