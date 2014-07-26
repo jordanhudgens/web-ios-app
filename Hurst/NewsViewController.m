60b86625ccfdacb9ff6a6adc31b3fa1c88427ea6
Jordan Hudgens <jordan@wow.com>
2014-07-25 22:41:59 -0500

    added webview to include news feed

---
+++ Hurst/NewsViewController.m
@@ -0,0 +1,47 @@
+//
+//  NewsViewController.m
+//  Hurst
+//
+//  Created by Jordan Hudgens on 7/25/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import "NewsViewController.h"
+
+@implementation NewsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    NSString *urlAddress = @"http://hurst.jordanhudgens.com/";
+    NSURL *url = [NSURL URLWithString:urlAddress];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    [self.webView loadRequest:request];
+    
+    [self.view addSubview:self.webView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self.webView reload];
+}
+
+
+
+@end
