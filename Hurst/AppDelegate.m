6f8ce59691be64e6ffec7bb15ca31cb53204f3fc
Jordan Hudgens <jordan@wow.com>
2014-08-05 21:10:08 -0700

    added the ability for push notification badges to be cleared

--- Hurst/AppDelegate.m
+++ Hurst/AppDelegate.m
@@ -28,8 +28,11 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    if (currentInstallation.badge != 0) {
+        currentInstallation.badge = 0;
+        [currentInstallation saveEventually];
+    }
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -68,5 +71,6 @@ didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 didReceiveRemoteNotification:(NSDictionary *)userInfo {
     [PFPush handlePush:userInfo];
 }
+
 
 @end
